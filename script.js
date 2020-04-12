e2826eba605feeb5f9e1449d39263caf599c2094
Athimoolam <maxat@amazon.com>
2020-04-11 18:35:44 -0700

    password Generator

---
+++ script.js
@@ -0,0 +1,105 @@
+
+character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+
+number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+let choices;
+
+alpha2 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+let generateBtn = document.querySelector("#generate");
+
+generateBtn.addEventListener("click", function () {
+    password = generatePassword();
+    document.querySelector("#password").placeholder = password;
+});
+
+// Assignment Code
+function generatePassword() {
+   
+    let passwordLength = parseInt(prompt("How long would you like your password to be ? Choose between 8 and 128"));
+    
+    while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) passwordLength = Number(prompt("Length must be 8-128 characters. How many characters would you like your password to be?"));
+
+   
+        
+        let confirmNumber = confirm("Will this contain numbers?");
+        let confirmCharacter = confirm("Will this contain special characters?");
+        let confirmUppercase = confirm("Will this contain Uppercase letters?");
+        let confirmLowercase = confirm("Will this contain Lowercase letters?");
+    
+
+
+    if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
+        choices = alert("You must choose a criteria!");
+
+    }
+   
+    else if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
+
+        choices = character.concat(number, alpha, alpha2);
+    }
+    // Else if for 3 positive options
+    else if (confirmCharacter && confirmNumber && confirmUppercase) {
+        choices = character.concat(number, alpha2);
+    }
+    else if (confirmCharacter && confirmNumber && confirmLowercase) {
+        choices = character.concat(number, alpha);
+    }
+    else if (confirmCharacter && confirmLowercase && confirmUppercase) {
+        choices = character.concat(alpha, alpha2);
+    }
+    else if (confirmNumber && confirmLowercase && confirmUppercase) {
+        choices = number.concat(alpha, alpha2);
+    }
+    // Else if for 2 positive options 
+    else if (confirmCharacter && confirmNumber) {
+        choices = character.concat(number);
+
+    } else if (confirmCharacter && confirmLowercase) {
+        choices = character.concat(alpha);
+
+    } else if (confirmCharacter && confirmUppercase) {
+        choices = character.concat(alpha2);
+    }
+    else if (confirmLowercase && confirmNumber) {
+        choices = alpha.concat(number);
+
+    } else if (confirmLowercase && confirmUppercase) {
+        choices = alpha.concat(alpha2);
+
+    } else if (confirmNumber && confirmUppercase) {
+        choices = number.concat(alpha2);
+    }
+    // Else if for 1 positive option
+    else if (confirmCharacter) {
+        choices = character;
+    }
+    else if (confirmNumber) {
+        choices = number;
+    }
+    else if (confirmLowercase) {
+        choices = alpha;
+    } ;
+   
+
+  
+    let passwordText = [];
+
+ 
+    for (let i = 0; i < passwordLength; i++) {
+        let pickChoices = choices[Math.floor(Math.random() * choices.length)];
+        passwordText.push(pickChoices);
+    }
+  // adding join("") cause i was getting commas without this 
+    let password = passwordText.join("");
+    writePassword(password);
+    return password;
+}
+
+function writePassword(password) {
+    document.getElementById("password").value = password;
+
+}
